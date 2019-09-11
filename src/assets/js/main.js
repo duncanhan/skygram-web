a88bf24eaa62fdc2d1fdbdb4ff73dd680a12dd24
Paul A. Tumwine <ptumwine@mum.edu>
2019-09-10 22:20:37 -0500

    Finish up dashboard layout

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,25 +1,25 @@
 	jQuery(document).ready(function($){
 		"use strict";
-		
+
 		var demo1 = $("#tada-slider").slippry({
 			speed: 1000,
 			pager:false,
 			captions:false
-		});		
-		
+		});
+
 		/* PRELOADER */
 		jQuery("#preloader-container").delay(2000).fadeOut("slow");
-		
+
 		/* STICKY MENU */
 		var stickyTop = $('nav.menu-sticky').offset().top;
 		$(window).scroll(function(){
-			if ($(this).scrollTop() >= stickyTop) {
+			if ($(this).scrollTop() > 0) {
 				$('nav').addClass('sticky');
 			} else {
 				$('nav').removeClass('sticky');
 			}
 		})
-	
+
 		/* BACK TO TOP */
 		$(window).scroll(function(){
 			if ($(this).scrollTop() > 50) {
@@ -29,10 +29,10 @@
 			}
 		});
 		$('.backtotop').click(function(){
-			$("html, body").animate({ scrollTop: 0 }, 500);        
+			$("html, body").animate({ scrollTop: 0 }, 500);
 			return false;
-		});	
-		
+		});
+
 		$('.open-menu-responsive').click(function(){
 			$('.menu-responsive').css('display','block');
 			$('.close-menu-responsive').css('display','block');
@@ -44,5 +44,5 @@
 			$('.close-menu-responsive').css('display','none');
 			$('.open-menu-responsive').css('display','block');
 			$('header').removeClass('header-menu-open');
-		});				
+		});
 	});
