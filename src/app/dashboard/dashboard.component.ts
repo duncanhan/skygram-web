5ba496398c9267eec4e570456786ef9f04f14c37
Duncan Sanya <cytrus.duncan@gmail.com>
2019-09-13 12:05:34 -0500

    Cleanup Post DATA that was introducing bugs
    Deleted

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,34 +8,6 @@ import {Post} from '../models/post-model';
 })
 export class DashboardComponent implements OnInit {
   posts: Post[] = [
-    {
-      id: 1,
-      title: 'First Post',
-      date: new Date(),
-      location: 'assets/img/img-post-1.jpg',
-      likes: null
-    },
-    {
-      id: 2,
-      title: 'Second Post',
-      date: new Date(),
-      location: 'assets/img/img-post-2.jpg',
-      likes: null
-    },
-    {
-      id: 3,
-      title: 'Third Post',
-      date: new Date(),
-      location: 'assets/img/img-post-3.jpg',
-      likes: null
-    },
-    {
-      id: 4,
-      title: 'Fourth Post',
-      date: new Date(),
-      location: 'assets/img/img-post-4.jpg',
-      likes: null
-    }
   ];
   constructor() { }
 
