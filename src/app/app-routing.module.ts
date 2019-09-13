19a3b0fa4895ef84dd4ce9555d7e7547cd85ff97
Paul A. Tumwine <ptumwine@mum.edu>
2019-09-13 14:28:44 -0500

    Add other user profile display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'user', component: UserComponent},
+  { path: 'user/:username', component: UserComponent },
   {path: 'dashboard', component: DashboardComponent },
   {path: 'registration', component: RegistrationComponent},
 ];
