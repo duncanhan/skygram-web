a36f030ac84cec7319d65993aaa36372c1dbb9e6
Ha Phuc <sfg7896@gmail.com>
2019-09-13 10:50:30 -0500

    add dropdown menu to avatar, logout function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   token: string = localStorage.getItem('token');
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  logOut() {
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('').then(() => window.location.reload());
+  }
 }
