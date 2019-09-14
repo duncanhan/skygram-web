569657930407c62ff9745b2af153a8e62569eeeb
Tubaleviao <alvaro.prata.filho@gmail.com>
2019-09-14 12:45:53 -0500

    user pic modal display

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,7 +9,8 @@ import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  templateUrl: './user.component.html'
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
   username: string;
