361b66270c98ac476d6bec8839b47a0d5b19b3ea
Duncan Sanya <cytrus.duncan@gmail.com>
2019-09-15 22:01:15 -0500

    Temporary data, loaded data from server

--- src/app/reporting/users/users.component.ts
+++ src/app/reporting/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {environment} from "../../../environments/environment";
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -8,42 +9,45 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  uploadForm: FormGroup;
   headers = new HttpHeaders({
     Authorization: 'Bearer ' + localStorage.getItem('token')
   });
-  usersData: any[];
-  postsData: any[];
-  lables: any[];
-  constructor(private httpClient: HttpClient) { }
 
-  ngOnInit() {
+  public usersData = [1, 0, 2, 1, 0, 3, 0];
+  public postsData = [5, 10, 17, 6, 10, 4, 20];
+  public lables = ['2019-09-10', '2019-09-10', '2019-09-10', '2019-09-10', '2019-09-10', '2019-09-10', '2019-09-10'];
+
+  constructor(private httpClient: HttpClient,private formBuilder: FormBuilder) {
+    this.lables = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    this.usersData = [65, 59, 80, 81, 56, 55, 40];
+    this.postsData = [28, 48, 40, 19, 86, 27, 90];
   }
 
-  getReport(): void {
-    const url = environment.url + '/reports?from=2019-09-01&to=2019-10-30';
+  ngOnInit() {
+    this.uploadForm = this.formBuilder.group({
+      from: '',
+      to: ''
+    });
+  }
+
+  onSubmit(): void {
+    const url = environment.url + '/reports?from='+this.uploadForm.value.from+'&to='+this.uploadForm.value.to;
+    console.log(url);
     this.httpClient.get<ReportResponse>(url, {headers: this.headers}).subscribe(
       response => {
         if (response.code === 200) {
-          response.data.forEach(function(element) {
-            console.log(element);
-            this.usersData.push(element.num_of_registration);
-            this.postsData.push(element.num_of_posts);
-            this.lables.push(element.date);
-          });
+          let data = response.data;
+          for(let i=0; i<data.length;i++){
+            this.usersData.push(data[i].num_of_registration);
+            this.postsData.push(data[i].num_of_posts);
+            this.lables.push(data[i].date);
+          }
         }
       }, error => {
         this.handleError(error);
       });
   }
-// {
-//   "code": 200,
-//   "message": "success",
-//   "data": [
-//     {
-//       "date": "2019-09-01",
-//       "num_of_registration": 0,
-//       "num_of_posts": 0
-//     },
 
   public barChartOptions = {
     scaleShowVerticalLines: false,
@@ -54,19 +58,20 @@ export class UsersComponent implements OnInit {
       }
     }
   };
-  public daysChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+
+  public daysChartLabels = this.lables;
   public barChartType = 'line';
   public barChartLegend = true;
   public daysUsersAndPostsChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Users'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Posts'}
+    {data: this.usersData, label: 'Users'},
+    {data: this.postsData, label: 'Posts'}
   ];
-
 
   handleError(error: string): void {
     console.log('Could not fetch content from server: ' + JSON.stringify(error));
   }
 }
+
 export interface ReportResponse {
   code: number;
   message: string;
@@ -74,7 +79,7 @@ export interface ReportResponse {
 }
 
 export interface ReportData {
-      date: Date,
+      date: string,
       num_of_registration: number,
       num_of_posts: number
 }
